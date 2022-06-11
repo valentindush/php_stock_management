744913c1e7d092a47b14352aa135d284d0cae1c4
valentindush <codesvalentin@gmail.com>
2022-06-11 12:29:31 +0200

    fx

--- store/oop/config.php
+++ store/oop/config.php
@@ -1,4 +1,12 @@
 <?php
 
     //DB CONFIGURATION
+
+    $host = "localhost";
+    $db = "stock";
+    $password = "";
+    $username = "root";
+
+    $connection = mysqli_connect($host,$username,$password,$db) or die("Connection failed")
+
 ?>
--- store/oop/crud.php
+++ store/oop/crud.php
@@ -1,9 +1,42 @@
 <?php
-
+    include "config.php";
     class Crud {
 
-        public function insert($table){
+        public $table;
+        private $conn = $connection;
+        public function __construct($table){
+            $this->table = $table;
+        }
 
+        public function insert($fields,$vals){
+            $cols = "";
+            $values = "";
+            
+            for($i = 0; $i < count($fields); $i++){
+
+                $cols +=" ".$i;
+            }
+
+            for($i = 0; $i < count($vals); $i++){
+
+                $values +=" ".$i;
+            }
+
+            $sql = "INSERT INTO ".$this->table."($cols) values($cols)";
+
+            $query = mysqli_query($this->conn,$sql);
+
+            if($query){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        public function update($fields,$vals){
+            $cols= "";
+            $values = "";
+            
+            
         }
     }
 
