9f573dfca2f3a03cbddec751be5adcacd0637cb2
valentindush <codesvalentin@gmail.com>
2022-06-11 09:40:19 +0200

    Setup

---
+++ store/oop/crud.php
@@ -0,0 +1,11 @@
+<?php
+
+    class Crud {
+
+        public function insert($table){
+
+        }
+    }
+
+    // $sql = "INSERT INTO users (name, email, password) VALUES ('John', 'codesval', '123456')";
+?>
