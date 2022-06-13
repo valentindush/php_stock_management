6581a9ced217d808bced188f2ca7bb63020bd3ec
valentindush <codesvalentin@gmail.com>
2022-06-13 20:27:28 +0200

    Added getAll and getOne

--- store/oop/crud.php
+++ store/oop/crud.php
@@ -1,11 +1,12 @@
 <?php
-    include "config.php";
+    include 'config.php';
     class Crud {
 
         public $table;
-        private $conn = $connection;
+        private $conn;
         public function __construct($table){
             $this->table = $table;
+            $this->$conn = $connection;
         }
 
         public function insert($fields,$vals){
@@ -42,18 +43,55 @@
                 $update_sql = join(",",$update_arr);
                 $sql = "ALTER TABLE ".$this->table."SET $update_sql WHERE $condition";
                 
+                $query = mysqli_query($this->conn,$sql);
+                if($query){
+                    return true;
+                }else{
+                    return false;
+                }
             }
             
             
             
         }
 
-        public function delete($id,$condition){
+        public function delete($condition){
             $sql = "DELETE FROM ".$this->table." WHERE ".$condition;
+            $query =  mysqli_query($this->conn,$sql);
+            if($query){
+                return true;
+            }else{
+                return false;
+            }
         }
 
         public function search($col,$val){
             $sql = "SELECT * FROM ".$this->table." WHERE ".$col." like %".$val."%";
+        }
+
+        public function getAll($col){
+            $sql = "SELECT * FROM ".$this->table;
+            $query = mysqli_query($this->conn,$sql);
+            $data = [];
+
+            if($query){
+                while($row = mysqli_fetch_assoc($query)){
+                    array_push($data,$row);
+                }
+                return $data;
+            }else{
+                return false;
+            }
+        }
+
+        public function getOne($col,$val){
+            $sql = "SELECT * FROM ".$this->table." WHERE ".$col." = ".$val;
+            $query = mysqli_query($this->conn,$sql);
+            if($query){
+                return $query;
+            }else{
+                return false;
+            }
         }
 
     }
