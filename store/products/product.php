fa722b16a0c5a8d6dd2b67bffba917395b16fb93
valentindush <codesvalentin@gmail.com>
2022-06-14 08:00:02 +0200

    added table

--- store/products/product.php
+++ store/products/product.php
@@ -34,12 +34,45 @@
 
             </div>
             <div class=" mt-5 content-center w-full">
+
                 <table class="w-[90%]  mt-10 rounded-xl table-auto border-collapse border-spacing-6">
+                    <div class="search bg-white w-[90%] flex items-center p-2 rounded-full relative justify-between">
+                        <i class="fa-solid fa-magnifying-glass absolute left-8 text-gray-500"></i>
+                        <input type="text" placeholder="Search products" class="p-2 bg-slate-100 rounded-full pl-12 outline-none border-none focus:border-red-400">
+                        <button class="border-2 border-red-400 p-2 px-4 text-sm rounded-full">New product</button>
+                    </div>
                     <thead>
                     
                     </thead>
                     <tbody class="border-collapse border-spacing-6">
-                    <tr class="p-2 shadow-md rounded-lg bg-white cursor-pointer">
+            
+                    <tr class="p-2 shadow-md rounded-lg bg-white mt-3">
+                        <td class="p-3"><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-select-2019-family?wid=882&hei=1058&fmt=jpeg&qlt=90&.v=1567022175704" alt="stuff" class="w-[100px] h-[100px] object-cover" ></td>
+                        <td class="flex justify-between  ">
+                            <div class="ml-5 pt-6">
+                                <p class="font-semi-bold text-black text-lg">Product Name</p>
+                                <p class="font-medium text-gray-700 text-sm mt-3">sto velit, id reprehenderit excepturi veniam, rem totam voluptatum consectetur doloremque vero, nemo pariatur qui!</p>
+                            </div>
+                            <div class="flex gap-3 items-center h-full pt-12">
+                                <button class="bg-slate-500 w-[100px] h-[33px] mr-5 flex gap-4 items-center justify-center text-white"><i class="fa-solid fa-pen"></i>Edit</button>
+                                <button class=" bg-red-400 w-[100px] h-[33px] mr-5 flex gap-4 items-center justify-center text-white"><i class="fa-solid fa-trash"></i>Delete</button>
+                            </div>
+                        </td>               
+                    </tr>
+                    <tr class="p-2 shadow-md rounded-lg bg-white mt-3">
+                        <td class="p-3"><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-select-2019-family?wid=882&hei=1058&fmt=jpeg&qlt=90&.v=1567022175704" alt="stuff" class="w-[100px] h-[100px] object-cover" ></td>
+                        <td class="flex justify-between  ">
+                            <div class="ml-5 pt-6">
+                                <p class="font-semi-bold text-black text-lg">Product Name</p>
+                                <p class="font-medium text-gray-700 text-sm mt-3">sto velit, id reprehenderit excepturi veniam, rem totam voluptatum consectetur doloremque vero, nemo pariatur qui!</p>
+                            </div>
+                            <div class="flex gap-3 items-center h-full pt-12">
+                                <button class="bg-slate-500 w-[100px] h-[33px] mr-5 flex gap-4 items-center justify-center text-white"><i class="fa-solid fa-pen"></i>Edit</button>
+                                <button class=" bg-red-400 w-[100px] h-[33px] mr-5 flex gap-4 items-center justify-center text-white"><i class="fa-solid fa-trash"></i>Delete</button>
+                            </div>
+                        </td>               
+                    </tr>
+                    <tr class="p-2 shadow-md rounded-lg bg-white mt-3">
                         <td class="p-3"><img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone11-select-2019-family?wid=882&hei=1058&fmt=jpeg&qlt=90&.v=1567022175704" alt="stuff" class="w-[100px] h-[100px] object-cover" ></td>
                         <td class="flex justify-between  ">
                             <div class="ml-5 pt-6">
